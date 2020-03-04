a1f19962b47670dc3c9a33ba569a5828c010759c
sergey-ku <kurgak.ss@gmail.com>
2020-03-05 02:27:57 +1100

    Day5 part2 solution, small refactoring

--- 2015/Day5/Day5.hs
+++ 2015/Day5/Day5.hs
@@ -1,15 +1,18 @@
+{-# LANGUAGE FlexibleContexts #-}
+
 import Text.Parsec
-import Data.Text (Text)
-import qualified Data.Text as T (lines)
-import qualified Data.Text.IO as TIO
+import Data.ByteString.Char8 (ByteString)
+import qualified Data.ByteString.Char8 as BS (split, readFile)
 import Text.Parsec.Char (endOfLine)
 import Data.Bifunctor (first, second)
 
 input = "./day5_input.txt"
 
-type StringState = (Int, Bool) -- (number of vowels, is there a double letter)
+type State1 = (Int, Bool)  -- (number of vowels; is there a double letters)
+type State2 = (Bool, Bool) -- (is there a pair that appear twice; is there a letter between identical letters)
 
-type Pars = Parsec Text StringState
+type Parser1 = Parsec ByteString State1
+type Parser2 = Parsec ByteString State2
 
 deprecatedStrings :: [String]
 deprecatedStrings = [ "ab"
@@ -17,44 +20,59 @@ deprecatedStrings = [ "ab"
                     , "pq"
                     , "xy"
                     ]
-                   
-deprecate :: String -> Pars Bool
-deprecate str = try $ string str >> return True
 
-deprecated :: Pars Bool
+deprecated :: Parser1 Bool
 deprecated = choice $ map (deprecate) deprecatedStrings
+  where deprecate str = try (string str) >> return True
 
-vowel :: Pars ()
+vowel :: Parser1 ()
 vowel = oneOf "aouie" >> modifyState (first (+1))
 
-doubleLetter :: Pars ()
-doubleLetter = do 
-            lookAhead $ letter >>= char
-            modifyState (fmap (const True))
+doubleLetter :: Parser1 ()
+doubleLetter = do lookAhead $ letter >>= char
+                  modifyState $ second (const True)
 
-rules :: Pars ()
-rules = do
-            isBad <- deprecated <|> return False
-            if isBad
-              then unexpected "Deprecated string"
-              else do 
-                    try doubleLetter <|> return ()
-                    vowel <|> (() <$ anyChar)
+pairTwice :: Char -> Parser2 ()
+pairTwice c = do lookAhead $ anyChar >>= \c' -> anyChar `manyTill` (try (string [c,c']))
+                 modifyState $ first (const True)
 
-niceString :: Pars Bool
-niceString = do 
-            (i, b) <- many rules >> getState
+betweenChar :: Char -> Parser2 ()
+betweenChar c = do lookAhead $ anyChar >> char c
+                   modifyState $ second (const True)
+                    
+rules1 :: Parser1 ()
+rules1 = do
+        isBad <- deprecated <|> return False
+        if isBad
+          then unexpected "Deprecated string"
+          else do 
+               try doubleLetter <|> return ()
+               vowel <|> (() <$ anyChar)
+
+rules2 :: Parser2 ()
+rules2 = do
+        c <- anyChar
+        (try (pairTwice c))   <|> return ()
+        (try (betweenChar c)) <|> return ()
+        
+                   
+niceString1 :: Parser1 Bool
+niceString1 = do
+            (i, b) <- many rules1 >> getState
             return $ and [i > 2, b]
-                       
 
+niceString2 :: Parser2 Bool
+niceString2 = do
+            (x,y) <- many rules2 >> getState
+            return $ x && y
                
-getCount :: Text -> Int
-getCount = length . filter (== valid) . map isValidString . T.lines
+getCount :: Parsec ByteString st Bool -> st -> ByteString -> Int
+getCount p st = length . filter (== valid) . map isValidString . BS.split '\n'
   where valid         = Right True  
-        initState     = (0,False)
-        isValidString = runParser niceString initState ""
+        isValidString = runParser p st ""
+        
+solveWith :: Show a => FilePath -> (ByteString -> a) -> IO ()
+solveWith i f = BS.readFile i >>= (print . f)
 
-solveWith :: Show a => (Text -> a) -> FilePath -> IO ()
-solveWith f i = TIO.readFile i >>= (print . f)
-
-main_part1 = solveWith getCount input
+main_part1 = input `solveWith` (getCount niceString1 (0,False))
+main_part2 = input `solveWith` (getCount niceString2 (False,False))
