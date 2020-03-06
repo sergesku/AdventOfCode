67522651f782e0adf565511e77ab750c8db7484b
sergey-ku <kurgak.ss@gmail.com>
2020-03-06 22:46:34 +1100

    day7: solve part1

--- 2015/Day7/Day7.hs
+++ 2015/Day7/Day7.hs
@@ -1,15 +1,17 @@
 import Data.Word
 import Data.Bits
-import qualified Data.Map as M
+import qualified Data.Map.Strict as M
 import Control.Monad.State
 import Text.Parsec hiding (State)
 import Data.ByteString.Char8 (ByteString)
+import qualified Data.ByteString.Char8 as BS
 import Data.Foldable (asum)
+import Data.Either (fromRight)
 
 input = "day7_input.txt"
 
-type Instruction = (String, Signal)
-type WireMap = M.Map String Signal
+type Connection = (String, Signal)
+type ConnectionMap = M.Map String Signal
 type Parser = Parsec ByteString ()
 
 data Signal = Value Word16
@@ -21,7 +23,7 @@ data Signal = Value Word16
             | Not Signal
             deriving (Eq, Show)
 
-evalSignal :: Signal -> State WireMap Word16
+evalSignal :: Signal -> State ConnectionMap Word16
 evalSignal (Value i)  = return i
 evalSignal (And s1 s2)  = (.&.) <$> (evalSignal s1) <*> (evalSignal s2)
 evalSignal (Or s1 s2)   = (.|.) <$> (evalSignal s1) <*> (evalSignal s2)
@@ -29,9 +31,9 @@ evalSignal (Lshift s i) = (`shiftL` i) <$> (evalSignal s)
 evalSignal (Rshift s i) = (`shiftR` i) <$> (evalSignal s)
 evalSignal (Not s)      = complement   <$> (evalSignal s)
 evalSignal (Wire str) = do
-                        mp       <- get
+                        mp  <- get
                         let (Just x) = M.lookup str mp
-                        signal   <- evalSignal x
+                        signal <- evalSignal x
                         modify (M.insert str (Value signal))
                         return signal
 
@@ -71,8 +73,19 @@ notSignal = Not <$> (string "NOT " >> wire)
 signal :: Parser Signal
 signal = asum $ try <$> [notSignal, andSignal, orSignal, lShiftSignal, rShiftSignal, simpleSignal]
 
-instruction :: Parser Instruction
-instruction = do s <- signal
-                 arrow
-                 w <- many1 anyChar
-                 return (w,s)
+connection :: Parser Connection
+connection = do s <- signal
+                arrow
+                w <- anyChar `manyTill` (() <$ endOfLine <|> eof)
+                return (w,s)
+
+connectionMap :: Parser ConnectionMap
+connectionMap = M.fromList <$> many connection
+
+solveWire :: String -> ByteString -> Word16 
+solveWire str = evalState (evalSignal (Wire str)) . fromRight M.empty . parse connectionMap ""
+
+solveWith :: Show a => FilePath -> (ByteString -> a) -> IO ()
+solveWith i f = BS.readFile i >>= print . f
+
+main_part1 = input `solveWith` (solveWire "a")
