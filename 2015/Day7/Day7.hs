210ad5e033177c07f3cbb469fb347a1266a49d9d
sergey-ku <kurgak.ss@gmail.com>
2020-03-06 17:45:50 +1100

    day7: add parser to soulution

--- 2015/Day7/Day7.hs
+++ 2015/Day7/Day7.hs
@@ -2,9 +2,15 @@ import Data.Word
 import Data.Bits
 import qualified Data.Map as M
 import Control.Monad.State
+import Text.Parsec hiding (State)
+import Data.ByteString.Char8 (ByteString)
+import Data.Foldable (asum)
 
-type Wire = String
-type WireMap = M.Map Wire Signal
+input = "day7_input.txt"
+
+type Instruction = (String, Signal)
+type WireMap = M.Map String Signal
+type Parser = Parsec ByteString ()
 
 data Signal = Value Word16
             | Wire String
@@ -28,5 +34,45 @@ evalSignal (Wire str) = do
                         signal   <- evalSignal x
                         modify (M.insert str (Value signal))
                         return signal
-                         
-                         
+
+lexeme :: Parser a -> Parser a
+lexeme p = p <* spaces
+
+integer :: Parser Int
+integer = lexeme $ read <$> (many1 digit)
+                        
+arrow :: Parser ()
+arrow = lexeme (string "->") >> return ()
+
+wire :: Parser Signal
+wire = Wire <$> (anyChar `manyTill` space)
+
+value :: Parser Signal
+value = (Value . fromIntegral) <$> integer
+
+simpleSignal :: Parser Signal
+simpleSignal = value <|> wire
+
+andSignal :: Parser Signal
+andSignal = And <$> simpleSignal <*> (string "AND " *> simpleSignal)
+
+orSignal :: Parser Signal
+orSignal = Or <$> simpleSignal <*> (string "OR " *> simpleSignal)
+
+lShiftSignal :: Parser Signal
+lShiftSignal = Lshift <$> simpleSignal <*> (string "LSHIFT " *> integer)
+
+rShiftSignal :: Parser Signal
+rShiftSignal = Rshift <$> simpleSignal <*> (string "RSHIFT " *> integer)
+
+notSignal :: Parser Signal
+notSignal = Not <$> (string "NOT " >> wire)
+
+signal :: Parser Signal
+signal = asum $ try <$> [notSignal, andSignal, orSignal, lShiftSignal, rShiftSignal, simpleSignal]
+
+instruction :: Parser Instruction
+instruction = do s <- signal
+                 arrow
+                 w <- many1 anyChar
+                 return (w,s)
