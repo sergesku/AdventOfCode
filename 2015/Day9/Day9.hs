8f238f13b2ac0f841381209e62fab81e93da7469
sergey-ku <kurgak.ss@gmail.com>
2020-03-10 17:05:13 +1100

    add: Day9.hs solution part1

---
+++ 2015/Day9/Day9.hs
@@ -0,0 +1,54 @@
+import Text.Parsec                           
+import Data.Map                              (Map)
+import Data.Set                              (Set)
+import Data.List                             (permutations)
+import Control.Applicative                   (liftA2)
+import Data.Either                           (fromRight)
+import Data.ByteString.Char8                 (ByteString)
+import qualified Data.ByteString.Char8 as BS (split, readFile)
+import qualified Data.Map              as M  (empty, insert, lookup)
+import qualified Data.Set              as S  (empty, insert, elems)
+
+type Point   = String
+type Edge = (Point,Point)
+type Route   = [Point]
+type Graph   = (Map Edge Int, Set Point)
+type Parser  = Parsec ByteString Graph
+
+input = "day9_input.txt"                                   
+
+emptyGraph :: Graph
+emptyGraph = (M.empty, S.empty)
+                                   
+number :: Parser Int
+number = read <$> many1 digit
+                                   
+point :: Parser Point
+point = (:) <$> upper <*> (manyTill anyChar space)
+
+edge :: Parser Edge
+edge = (,) <$> point <*> (string "to " *> point)
+
+entry :: Parser ()
+entry = do
+        e@(p1,p2) <- edge <* string "= "
+        n         <- number
+        (m,s)     <- getState
+        let newSet = S.insert p1 . S.insert p2 $ s
+            newMap = M.insert e n . M.insert (p2,p1) n $ m
+        putState (newMap, newSet)
+
+graph :: Parser Graph
+graph = sepEndBy1 entry endOfLine >> getState
+
+minDistance :: Graph -> Int
+minDistance (m,s) = minimum $ do
+        route <- permutations . S.elems $ s
+        let routeToEdges = liftA2 zip id tail
+            Just ints    = traverse (`M.lookup` m) . routeToEdges $ route 
+        return $ sum ints
+
+solveWith :: Show a => FilePath -> (ByteString -> a) -> IO ()
+solveWith i f = BS.readFile i >>= print . f
+
+main_part1 = input `solveWith` (minDistance . fromRight emptyGraph . runParser graph emptyGraph "")
