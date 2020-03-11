5f31ba5e19635ae26a7259c0872239c0cb72b8d1
sergey-ku <kurgak.ss@gmail.com>
2020-03-12 00:01:22 +1100

    refact: day11 silution

--- 2015/Day11/Day11.hs
+++ 2015/Day11/Day11.hs
@@ -1,18 +1,18 @@
-import Data.Char
+import Control.Applicative (liftA2)
 
 input = "hxbxwxba"
 
 succString :: String -> String
 succString ""       = ""
+succString ('h':xs) = 'j' : (succString xs)
+succString ('k':xs) = 'm' : (succString xs)
+succString ('n':xs) = 'p' : (succString xs)
 succString ('z':xs) = 'a' : (succString xs)
 succString (x:xs)   = (succ x) : xs 
 
 hasStraight :: String -> Bool
 hasStraight (z:rest@(y:x:_)) = ((z == succ y) && (y == succ x)) || hasStraight rest
 hasStraight _ = False
-
-hasNoDeprecated :: String -> Bool
-hasNoDeprecated = not . any (`elem` "iol")
 
 hasDifferentPair :: Char -> String -> Bool
 hasDifferentPair c (x:y:rest) | x == c    = hasDifferentPair c (y:rest)
@@ -25,7 +25,7 @@ hasTwoPair (x:y:rest) = ((x == y) && (hasDifferentPair x rest)) || (hasTwoPair (
 hasTwoPair _ = False
 
 correctPassword :: String -> Bool
-correctPassword = and . sequenceA [hasStraight, hasNoDeprecated, hasTwoPair]
+correctPassword = liftA2 (&&) hasStraight hasTwoPair
 
 nextPassword :: String -> String
 nextPassword = reverse . head . filter (correctPassword) . drop 1 . iterate succString . reverse
