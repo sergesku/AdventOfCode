8f8e9a6bedd67ae68f533f86178d7cc741d72781
sergey-ku <kurgak.ss@gmail.com>
2020-03-18 15:37:10 +1100

    add: day8 solution part 2

--- 2015/Day8/Day8.hs
+++ 2015/Day8/Day8.hs
@@ -1,12 +1,21 @@
-
 input = "day8_input.txt"
 
-countDifference :: Int -> String -> Int
-countDifference n [] = n
-countDifference n ('\\' : '\\' : rest)        = countDifference (n + 1) rest
-countDifference n ('\\' : '"' : rest)         = countDifference (n + 1) rest
-countDifference n ('\\' : 'x' : _ : _ : rest) = countDifference (n + 3) rest
-countDifference n ('"' : rest )               = countDifference (n + 1) rest
-countDifference n (_ : rest)               = countDifference n rest
+differenceDecoded :: Int -> String -> Int
+differenceDecoded n [] = n
+differenceDecoded n ('\\' : '\\' : rest)        = differenceDecoded (n + 1) rest
+differenceDecoded n ('\\' : '"' : rest)         = differenceDecoded (n + 1) rest
+differenceDecoded n ('\\' : 'x' : _ : _ : rest) = differenceDecoded (n + 3) rest
+differenceDecoded n ('"'  : rest )              = differenceDecoded (n + 1) rest
+differenceDecoded n ( _  : rest)                = differenceDecoded n rest
 
-main = readFile input >>= print . sum . map (countDifference 0) . lines
+differenceEncoded :: Int -> String -> Int
+differenceEncoded n [] = n
+differenceEncoded n ('"' : rest)  = differenceEncoded (n + 1) rest
+differenceEncoded n ('\\' : rest) = differenceEncoded (n + 1) rest
+differenceEncoded n ( _ : rest )  = differenceEncoded n rest
+
+solveWith :: FilePath -> (String -> Int) -> IO ()
+solveWith file f = readFile file >>= print . sum . map f . lines
+
+main_part1 = input `solveWith` (differenceDecoded 0)
+main_part2 = input `solveWith` (differenceEncoded 2)
