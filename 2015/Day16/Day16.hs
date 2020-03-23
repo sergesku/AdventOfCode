ae8d71e764fc22043752e93b9b7c33fe782992ac
sergey-ku <kurgak.ss@gmail.com>
2020-03-23 12:00:55 +1100

    add: day16 solution part 2

--- 2015/Day16/Day16.hs
+++ 2015/Day16/Day16.hs
@@ -1,17 +1,11 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-{--
-Sue 102: pomeranians: 6, trees: 1, samoyeds: 4
-Sue 103: cars: 2, perfumes: 1, goldfish: 5
-Sue 104: goldfish: 2, cars: 8, pomeranians: 2
-Sue 105: goldfish: 6, vizslas: 0, trees: 10
-Sue 106: trees: 10, akitas: 10, pomeranians: 0
---}
 import Text.Parsec
-import Data.Map (Map)
-import Data.ByteString.Char8 (ByteString)
-import qualified Data.Map              as M
-import qualified Data.ByteString.Char8 as BS
+import Data.Function                         ((&))
+import Data.Map                              (Map)
+import Data.ByteString.Char8                 (ByteString)
+import qualified Data.Map              as M  (fromList, insert, lookup, keys)
+import qualified Data.ByteString.Char8 as BS (readFile)
 
 type Entry = (String, Int)
 type Parser = Parsec ByteString ()
@@ -22,21 +16,34 @@ data Sue = Sue { number :: Int
 
 input = "day16_input.txt"
                
-outputMFCSAM :: Map String Int
-outputMFCSAM = M.fromList [ ("children", 3)
-                          , ("cats", 7)
-                          , ("samoyeds", 2)
-                          , ("pomeranians", 3)
-                          , ("akitas", 0)
-                          , ("vizslas", 0)
-                          , ("goldfish", 5)
-                          , ("trees", 3)
-                          , ("cars", 2)
-                          , ("perfumes", 1)
-                          ]
+outputMFCSAM_1 :: Map String (Int -> Bool)
+outputMFCSAM_1 = M.fromList [ ("children", (== 3))
+                            , ("cats", (== 7))
+                            , ("samoyeds", (== 2))
+                            , ("pomeranians", (== 3))
+                            , ("akitas", (== 0))
+                            , ("vizslas", (== 0))
+                            , ("goldfish", (== 5))
+                            , ("trees", (== 3))
+                            , ("cars", (== 2))
+                            , ("perfumes", (== 1))
+                            ]
 
+outputMFCSAM_2 :: Map String (Int -> Bool)
+outputMFCSAM_2 = M.fromList [ ("children", (== 3))
+                            , ("cats", (> 7))
+                            , ("samoyeds", (== 2))
+                            , ("pomeranians", (< 3))
+                            , ("akitas", (== 0))
+                            , ("vizslas", (== 0))
+                            , ("goldfish", (< 5))
+                            , ("trees", (> 3))
+                            , ("cars", (== 2))
+                            , ("perfumes", (== 1))
+                            ]                       
+                                                  
 thing :: Parser String
-thing = choice $ map (try . string)(M.keys outputMFCSAM)
+thing = choice $ map (try . string)(M.keys outputMFCSAM_1)
 
 entry :: Parser Entry
 entry = (,)
@@ -54,16 +61,20 @@ sue = Sue
 allSues :: Parser [Sue]
 allSues = sue `sepEndBy` endOfLine
                           
-checkEntry :: Map String Int -> (String, Int) -> Bool
-checkEntry m (s,i) = case (==i) <$> (M.lookup s m) of
+checkEntry :: Map String (Int -> Bool) -> (String, Int) -> Bool
+checkEntry m (s,i) = case (i &) <$> (M.lookup s m) of
                        (Just True) -> True
                        _           -> False
 
-checkSue :: Map String Int -> Sue -> Bool
+checkSue :: Map String (Int -> Bool) -> Sue -> Bool
 checkSue m sue = and $ map (checkEntry m) (things sue)
 
 
-findSue :: Map String Int -> [Sue] -> Sue
+findSue :: Map String (Int -> Bool) -> [Sue] -> Sue
 findSue m = head . filter (checkSue m)
 
-main_part1 = BS.readFile input >>= print . fmap (number . findSue outputMFCSAM) . parse allSues ""
+solveWith :: FilePath -> Map String (Int -> Bool) -> IO ()
+solveWith file m = BS.readFile file >>= print . fmap (number . findSue m) . parse allSues ""
+
+main_part1 = input `solveWith` outputMFCSAM_1
+main_part2 = input `solveWith` outputMFCSAM_2
