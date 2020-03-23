7246e923ce2b4e43a7fbfeb4e31dceddb780ed22
sergey-ku <kurgak.ss@gmail.com>
2020-03-23 12:31:22 +1100

    add: day17 solution part 2

--- 2015/Day17/Day17.hs
+++ 2015/Day17/Day17.hs
@@ -1,8 +1,21 @@
+import Data.List     (sortOn, groupBy)
+import Data.Function (on)
 import Control.Monad (filterM)
 
+input :: FilePath
 input = "day17_input.txt"
 
 totalEggnog :: Int
 totalEggnog = 150
 
-main_part1 = readFile input >>= print . length . filter ((== totalEggnog) . sum) . filterM (\_ -> [True, False]) . fmap read . words
+combinations :: Int -> [Int] -> [[Int]]
+combinations i = filter ((== i) . sum) . filterM (\_ -> [True, False]) 
+
+minCombinations :: [[Int]] -> [[Int]]
+minCombinations = head . groupBy ((==) `on` length) . sortOn length
+
+solveWith :: FilePath -> ([Int] -> [[Int]]) -> IO ()
+solveWith file f = readFile file >>= print . length . f . fmap read . words
+
+main_part1 = input `solveWith` (combinations totalEggnog)
+main_part2 = input `solveWith` (minCombinations . combinations totalEggnog)
